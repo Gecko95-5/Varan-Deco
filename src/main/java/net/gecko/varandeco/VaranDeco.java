35b1f0c1b766de70cacac1a924f3277609f99037
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2025-04-17 20:49:42 -0300

    0.5.5-(Classiving Flammable, Oxidizable, Strippable Blocks)

--- src/main/java/net/gecko/varandeco/VaranDeco.java
+++ src/main/java/net/gecko/varandeco/VaranDeco.java
@@ -12,8 +12,7 @@ import net.gecko.varandeco.block.entity.DecoBlockEntities;
 import net.gecko.varandeco.item.DecoItemGroup;
 import net.gecko.varandeco.item.DecoItems;
 import net.gecko.varandeco.potion.DecoPotion;
-import net.gecko.varandeco.util.DecoCustomTrades;
-import net.gecko.varandeco.util.DecoTags;
+import net.gecko.varandeco.util.*;
 import net.gecko.varandeco.world.feature.DecoConfiguredFeatures;
 import net.gecko.varandeco.world.gen.DecoWorldGeneration;
 import net.minecraft.block.Blocks;
@@ -39,114 +38,10 @@ public class VaranDeco implements ModInitializer {
 		DecoWorldGeneration.generateWorldGen();
 		DecoCustomTrades.registerCustomTrades();
 		DecoPotion.registerPotions();
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.CACTUS_PLANKS,5,20);
-		FlammableBlockRegistry.getDefaultInstance().add(Blocks.CACTUS,5,5);
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.WOODEN_PLANKS,5,20);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.WOODEN_LOG,5,5);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.STRIPPED_WOODEN_LOG,5,5);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.WOODEN_WOOD,5,5);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.STRIPPED_WOODEN_WOOD,5,5);
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.YELLOW_TULIP,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.PURPLE_TULIP,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.MAGENTA_TULIP,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.BLACK_TULIP,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.GREEN_TULIP,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.BLUE_TULIP,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.CYAN_TULIP,60, 100);
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.BARBERTON_DAISY,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.BLUE_EYED_DAISY,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.GERBERA_DAISY,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.MICHAELMAS_DAISY,60, 100);
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.PUFFY_DANDELION,60, 100);
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.CALIFORNIA_POPPY,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.SALMON_POPPY,60, 100);
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.YELLOW_ORCHID,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.WHITE_ORCHID,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.PINK_ORCHID,60, 100);
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.ROSE,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.ENDER_ROSE,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.WITHER_ROSE_BUSH,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.ENDER_ROSE_BUSH,60, 100);
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.RED_SUNFLOWER,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.NOVA_STARFLOWER,60, 100);
-
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.PAEONIA,60, 100);
-		FlammableBlockRegistry.getDefaultInstance().add(DecoBlocks.LAVENDER,60, 100);
-
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.HEAVY_COPPER_BARS, DecoBlocks.WAXED_HEAVY_COPPER_BARS);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.EXPOSED_HEAVY_COPPER_BARS, DecoBlocks.WAXED_EXPOSED_HEAVY_COPPER_BARS);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.WEATHERED_HEAVY_COPPER_BARS, DecoBlocks.WAXED_WEATHERED_HEAVY_COPPER_BARS);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.OXIDIZED_HEAVY_COPPER_BARS, DecoBlocks.WAXED_OXIDIZED_HEAVY_COPPER_BARS);
-
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.HEAVY_COPPER_BARS, DecoBlocks.EXPOSED_HEAVY_COPPER_BARS);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.EXPOSED_HEAVY_COPPER_BARS, DecoBlocks.WEATHERED_HEAVY_COPPER_BARS);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.WEATHERED_HEAVY_COPPER_BARS, DecoBlocks.OXIDIZED_HEAVY_COPPER_BARS);
-
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.COPPER_BARS, DecoBlocks.WAXED_COPPER_BARS);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.EXPOSED_COPPER_BARS, DecoBlocks.WAXED_EXPOSED_COPPER_BARS);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.WEATHERED_COPPER_BARS, DecoBlocks.WAXED_WEATHERED_COPPER_BARS);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.OXIDIZED_COPPER_BARS, DecoBlocks.WAXED_OXIDIZED_COPPER_BARS);
-
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.COPPER_BARS, DecoBlocks.EXPOSED_COPPER_BARS);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.EXPOSED_COPPER_BARS, DecoBlocks.WEATHERED_COPPER_BARS);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.WEATHERED_COPPER_BARS, DecoBlocks.OXIDIZED_COPPER_BARS);
-
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.LIGHT_COPPER_BARS, DecoBlocks.WAXED_LIGHT_COPPER_BARS);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.EXPOSED_LIGHT_COPPER_BARS, DecoBlocks.WAXED_EXPOSED_LIGHT_COPPER_BARS);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.WEATHERED_LIGHT_COPPER_BARS, DecoBlocks.WAXED_WEATHERED_LIGHT_COPPER_BARS);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.OXIDIZED_LIGHT_COPPER_BARS, DecoBlocks.WAXED_OXIDIZED_LIGHT_COPPER_BARS);
-
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.LIGHT_COPPER_BARS, DecoBlocks.EXPOSED_LIGHT_COPPER_BARS);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.EXPOSED_LIGHT_COPPER_BARS, DecoBlocks.WEATHERED_LIGHT_COPPER_BARS);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.WEATHERED_LIGHT_COPPER_BARS, DecoBlocks.OXIDIZED_LIGHT_COPPER_BARS);
-
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.COPPER_LANTERN, DecoBlocks.WAXED_COPPER_LANTERN);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.EXPOSED_COPPER_LANTERN, DecoBlocks.WAXED_EXPOSED_COPPER_LANTERN);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.WEATHERED_COPPER_LANTERN, DecoBlocks.WAXED_WEATHERED_COPPER_LANTERN);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.OXIDIZED_COPPER_LANTERN, DecoBlocks.WAXED_OXIDIZED_COPPER_LANTERN);
-
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.COPPER_LANTERN, DecoBlocks.EXPOSED_COPPER_LANTERN);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.EXPOSED_COPPER_LANTERN, DecoBlocks.WEATHERED_COPPER_LANTERN);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.WEATHERED_COPPER_LANTERN, DecoBlocks.OXIDIZED_COPPER_LANTERN);
-
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.COPPER_SOUL_LANTERN, DecoBlocks.WAXED_COPPER_SOUL_LANTERN);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.EXPOSED_COPPER_SOUL_LANTERN, DecoBlocks.WAXED_EXPOSED_COPPER_SOUL_LANTERN);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.WEATHERED_COPPER_SOUL_LANTERN, DecoBlocks.WAXED_WEATHERED_COPPER_SOUL_LANTERN);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.OXIDIZED_COPPER_SOUL_LANTERN, DecoBlocks.WAXED_OXIDIZED_COPPER_SOUL_LANTERN);
-
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.COPPER_SOUL_LANTERN, DecoBlocks.EXPOSED_COPPER_SOUL_LANTERN);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.EXPOSED_COPPER_SOUL_LANTERN, DecoBlocks.WEATHERED_COPPER_SOUL_LANTERN);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.WEATHERED_COPPER_SOUL_LANTERN, DecoBlocks.OXIDIZED_COPPER_SOUL_LANTERN);
-
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.COPPER_CHAIN, DecoBlocks.WAXED_COPPER_CHAIN);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.EXPOSED_COPPER_CHAIN, DecoBlocks.WAXED_EXPOSED_COPPER_CHAIN);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.WEATHERED_COPPER_CHAIN, DecoBlocks.WAXED_WEATHERED_COPPER_CHAIN);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.OXIDIZED_COPPER_CHAIN, DecoBlocks.WAXED_OXIDIZED_COPPER_CHAIN);
-
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.COPPER_CHAIN, DecoBlocks.EXPOSED_COPPER_CHAIN);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.EXPOSED_COPPER_CHAIN, DecoBlocks.WEATHERED_COPPER_CHAIN);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.WEATHERED_COPPER_CHAIN, DecoBlocks.OXIDIZED_COPPER_CHAIN);
-
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.COPPER_WEIGHT_PRESSURE_PLATE, DecoBlocks.WAXED_COPPER_WEIGHT_PRESSURE_PLATE);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(EXPOSED_COPPER_WEIGHT_PRESSURE_PLATE, DecoBlocks.WAXED_EXPOSED_COPPER_WEIGHT_PRESSURE_PLATE);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.WEATHERED_COPPER_WEIGHT_PRESSURE_PLATE, DecoBlocks.WAXED_WEATHERED_COPPER_WEIGHT_PRESSURE_PLATE);
-		OxidizableBlocksRegistry.registerWaxableBlockPair(DecoBlocks.OXIDIZED_COPPER_WEIGHT_PRESSURE_PLATE, DecoBlocks.WAXED_OXIDIZED_COPPER_WEIGHT_PRESSURE_PLATE);
-
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.COPPER_WEIGHT_PRESSURE_PLATE, EXPOSED_COPPER_WEIGHT_PRESSURE_PLATE);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.EXPOSED_COPPER_WEIGHT_PRESSURE_PLATE, DecoBlocks.WEATHERED_COPPER_WEIGHT_PRESSURE_PLATE);
-		OxidizableBlocksRegistry.registerOxidizableBlockPair(DecoBlocks.WEATHERED_COPPER_WEIGHT_PRESSURE_PLATE, DecoBlocks.OXIDIZED_COPPER_WEIGHT_PRESSURE_PLATE);
-
-		StrippableBlockRegistry.register(DecoBlocks.WOODEN_LOG, DecoBlocks.STRIPPED_WOODEN_LOG);
-		StrippableBlockRegistry.register(DecoBlocks.WOODEN_WOOD, DecoBlocks.STRIPPED_WOODEN_WOOD);
+		DecoFlammableBlocks.registerFlammableBlocks();
+		DecoStrippableBlocks.registerStrippables();
+		DecoOxidizableBlocks.registerOxidizableBlocks();
+		DecoOxidizableBlocks.registerWaxableBlocks();
 
 		ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) ->
 						world != null && pos != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.getDefaultColor(),
--- src/main/java/net/gecko/varandeco/block/DecoBlocks.java
+++ src/main/java/net/gecko/varandeco/block/DecoBlocks.java
@@ -941,9 +941,11 @@ public class DecoBlocks {
     public static final Block MANGROVE_CRAFTING_TABLE = registerBlock("mangrove_crafting_table",
             new MangroveCraftingTableBlock(FabricBlockSettings.copyOf(Blocks.CRAFTING_TABLE).mapColor(MapColor.RED)));
     public static final Block CRIMSON_CRAFTING_TABLE = registerBlock("crimson_crafting_table",
-            new CrimsonCraftingTableBlock(FabricBlockSettings.copyOf(Blocks.CRAFTING_TABLE).mapColor(MapColor.DULL_PINK)));
+            new CrimsonCraftingTableBlock(FabricBlockSettings.copyOf(Blocks.CRAFTING_TABLE).mapColor(MapColor.DULL_PINK)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block WARPED_CRAFTING_TABLE = registerBlock("warped_crafting_table",
-            new WarpedCraftingTableBlock(FabricBlockSettings.copyOf(Blocks.CRAFTING_TABLE).mapColor(MapColor.TEAL)));
+            new WarpedCraftingTableBlock(FabricBlockSettings.copyOf(Blocks.CRAFTING_TABLE).mapColor(MapColor.TEAL)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block CACTUS_CRAFTING_TABLE = registerBlock("cactus_crafting_table",
             new CactusCraftingTableBlock(FabricBlockSettings.copyOf(Blocks.CRAFTING_TABLE).mapColor(MapColor.DARK_GREEN)));
     public static final Block WOODEN_CRAFTING_TABLE = registerBlock("wooden_crafting_table",
@@ -969,9 +971,11 @@ public class DecoBlocks {
     public static final Block MANGROVE_LADDER = registerBlock("mangrove_ladder",
             new LadderBlock(FabricBlockSettings.copyOf(Blocks.LADDER)));
     public static final Block CRIMSON_LADDER = registerBlock("crimson_ladder",
-            new LadderBlock(FabricBlockSettings.copyOf(Blocks.LADDER)));
+            new LadderBlock(FabricBlockSettings.copyOf(Blocks.LADDER)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block WARPED_LADDER = registerBlock("warped_ladder",
-            new LadderBlock(FabricBlockSettings.copyOf(Blocks.LADDER)));
+            new LadderBlock(FabricBlockSettings.copyOf(Blocks.LADDER)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block CACTUS_LADDER = registerBlock("cactus_ladder",
             new LadderBlock(FabricBlockSettings.copyOf(Blocks.LADDER)));
     public static final Block WOODEN_LADDER = registerBlock("wooden_ladder",
@@ -1398,9 +1402,11 @@ public class DecoBlocks {
     public static final Block MANGROVE_BARREL = registerBlock("mangrove_barrel",
             new BarrelBlock(FabricBlockSettings.copyOf(Blocks.BARREL).mapColor(MapColor.RED)));
     public static final Block CRIMSON_BARREL = registerBlock("crimson_barrel",
-            new BarrelBlock(FabricBlockSettings.copyOf(Blocks.BARREL).mapColor(MapColor.DULL_PINK)));
+            new BarrelBlock(FabricBlockSettings.copyOf(Blocks.BARREL).mapColor(MapColor.DULL_PINK)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block WARPED_BARREL = registerBlock("warped_barrel",
-            new BarrelBlock(FabricBlockSettings.copyOf(Blocks.BARREL).mapColor(MapColor.DARK_AQUA)));
+            new BarrelBlock(FabricBlockSettings.copyOf(Blocks.BARREL).mapColor(MapColor.DARK_AQUA)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block CACTUS_BARREL = registerBlock("cactus_barrel",
             new BarrelBlock(FabricBlockSettings.copyOf(Blocks.BARREL).mapColor(MapColor.PALE_GREEN)));
 
@@ -1433,9 +1439,11 @@ public class DecoBlocks {
     public static final Block MANGROVE_CARTOGRAPHY_TABLE = registerBlock("mangrove_cartography_table",
             new MangroveCartographyTableBlock(FabricBlockSettings.copyOf(Blocks.CARTOGRAPHY_TABLE)));
     public static final Block CRIMSON_CARTOGRAPHY_TABLE = registerBlock("crimson_cartography_table",
-            new CrimsonCartographyTableBlock(FabricBlockSettings.copyOf(Blocks.CARTOGRAPHY_TABLE)));
+            new CrimsonCartographyTableBlock(FabricBlockSettings.copyOf(Blocks.CARTOGRAPHY_TABLE)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block WARPED_CARTOGRAPHY_TABLE = registerBlock("warped_cartography_table",
-            new WarpedCartographyTableBlock(FabricBlockSettings.copyOf(Blocks.CARTOGRAPHY_TABLE)));
+            new WarpedCartographyTableBlock(FabricBlockSettings.copyOf(Blocks.CARTOGRAPHY_TABLE)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block CACTUS_CARTOGRAPHY_TABLE = registerBlock("cactus_cartography_table",
             new CactusCartographyTableBlock(FabricBlockSettings.copyOf(Blocks.CARTOGRAPHY_TABLE)));
 
@@ -1454,9 +1462,11 @@ public class DecoBlocks {
     public static final Block MANGROVE_SMITHING_TABLE = registerBlock("mangrove_smithing_table",
             new MangroveSmithingTableBlock(FabricBlockSettings.copyOf(Blocks.SMITHING_TABLE)));
     public static final Block CRIMSON_SMITHING_TABLE = registerBlock("crimson_smithing_table",
-            new CrimsonSmithingTableBlock(FabricBlockSettings.copyOf(Blocks.SMITHING_TABLE)));
+            new CrimsonSmithingTableBlock(FabricBlockSettings.copyOf(Blocks.SMITHING_TABLE)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block WARPED_SMITHING_TABLE = registerBlock("warped_smithing_table",
-            new WarpedSmithingTableBlock(FabricBlockSettings.copyOf(Blocks.SMITHING_TABLE)));
+            new WarpedSmithingTableBlock(FabricBlockSettings.copyOf(Blocks.SMITHING_TABLE)
+                    .sounds(BlockSoundGroup.NETHER_STEM)));
     public static final Block CACTUS_SMITHING_TABLE = registerBlock("cactus_smithing_table",
             new CactusSmithingTableBlock(FabricBlockSettings.copyOf(Blocks.SMITHING_TABLE)));
 
