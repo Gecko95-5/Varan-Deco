dd4da17dc65d1854f176fe74f86a04240d41fe11
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2025-05-11 15:09:46 -0400

    0.5.6-(Loot Table Fix)

---
+++ src/main/java/net/gecko/varandeco/util/DecoLootTableModifiers.java
@@ -0,0 +1,30 @@
+package net.gecko.varandeco.util;
+
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.gecko.varandeco.item.DecoItems;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
+
+public class DecoLootTableModifiers {
+    private static final Identifier PUFFERFISH_ID =
+            new Identifier("minecraft", "entities/pufferfish");
+
+    public static void modifyLootTables() {
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (PUFFERFISH_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.01f))
+                        .with(ItemEntry.builder(DecoItems.BUBBLE_ORB))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+    }
+}
