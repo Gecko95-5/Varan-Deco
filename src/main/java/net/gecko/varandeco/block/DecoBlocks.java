d7111cc4c5acaee5a98a2c2028329725675d9f33
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2025-06-27 22:11:35 -0300

    1.1.B-(Infested Block Creative Tab Reorientation)

--- src/main/java/net/gecko/varandeco/block/DecoBlocks.java
+++ src/main/java/net/gecko/varandeco/block/DecoBlocks.java
@@ -1184,10 +1184,10 @@ public class DecoBlocks {
                     .mapColor(MapColor.TERRACOTTA_WHITE).sounds(BlockSoundGroup.CALCITE)));
     public static final Block INFESTED_DRIPSTONE_BLOCK = registerBlock("infested_dripstone_block",
             new InfestedBlock(Blocks.DRIPSTONE_BLOCK, FabricBlockSettings.copyOf(Blocks.INFESTED_COBBLESTONE)
-                    .mapColor(MapColor.TERRACOTTA_BROWN).sounds(BlockSoundGroup.TUFF)));
+                    .mapColor(MapColor.TERRACOTTA_GRAY).sounds(BlockSoundGroup.DRIPSTONE_BLOCK)));
     public static final Block INFESTED_TUFF = registerBlock("infested_tuff",
             new InfestedBlock(Blocks.TUFF, FabricBlockSettings.copyOf(Blocks.INFESTED_COBBLESTONE)
-                    .mapColor(MapColor.TERRACOTTA_GRAY).sounds(BlockSoundGroup.DRIPSTONE_BLOCK)));
+                    .mapColor(MapColor.TERRACOTTA_BROWN).sounds(BlockSoundGroup.TUFF)));
 
     public static final Block INFESTED_GRANITE = registerBlock("infested_granite",
             new InfestedBlock(Blocks.GRANITE, FabricBlockSettings.copyOf(Blocks.INFESTED_COBBLESTONE)
@@ -1204,7 +1204,7 @@ public class DecoBlocks {
                     .mapColor(MapColor.OFF_WHITE)));
 
     public static final Block INFESTED_ANDESITE = registerBlock("infested_andesite",
-            new InfestedBlock(Blocks.DIORITE, FabricBlockSettings.copyOf(Blocks.INFESTED_COBBLESTONE)
+            new InfestedBlock(Blocks.ANDESITE, FabricBlockSettings.copyOf(Blocks.INFESTED_COBBLESTONE)
                     .mapColor(MapColor.STONE_GRAY)));
     public static final Block INFESTED_POLISHED_ANDESITE = registerBlock("infested_polished_andesite",
             new InfestedBlock(Blocks.POLISHED_ANDESITE, FabricBlockSettings.copyOf(Blocks.INFESTED_COBBLESTONE)
