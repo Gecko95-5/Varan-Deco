e72e462105c875d020b41b1433abd4c75958af37
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2025-06-30 17:47:41 -0300

    1.1.C-(Pink Petals Sus Stew Added)

--- src/main/java/net/gecko/varandeco/item/DecoFoodComponents.java
+++ src/main/java/net/gecko/varandeco/item/DecoFoodComponents.java
@@ -6,13 +6,16 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Items;
 
 public class DecoFoodComponents {
-    public static final FoodComponent LILAC_STEW = createStew(6).alwaysEdible()
+    public static final FoodComponent LILAC_STEW = createStew().alwaysEdible()
             .statusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, 100, 0), 1.0F).build();
 
-    public static final FoodComponent MIGHTY_LAVENDER_STEW = createStew(6).alwaysEdible()
+    public static final FoodComponent MIGHTY_LAVENDER_STEW = createStew().alwaysEdible()
             .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 260, 0), 1.0F).build();
 
-    private static FoodComponent.Builder createStew(int hunger) {
-        return new FoodComponent.Builder().nutrition(hunger).saturationModifier(0.6F).usingConvertsTo(Items.BOWL);
+    public static final FoodComponent PINK_PETALS_STEW = createStew().alwaysEdible()
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 60, 0), 1.0F).build();
+
+    private static FoodComponent.Builder createStew() {
+        return new FoodComponent.Builder().nutrition(6).saturationModifier(0.6F).usingConvertsTo(Items.BOWL);
     }
 }
