c39aa0dc3212f5f68fe74501cf414a76de93b984
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2025-07-21 21:30:36 -0300

    1.3.B-(New Flower and Advancements)

--- src/main/java/net/gecko/varandeco/datagen/DecoItemTagProvider.java
+++ src/main/java/net/gecko/varandeco/datagen/DecoItemTagProvider.java
@@ -237,7 +237,9 @@ public class DecoItemTagProvider extends FabricTagProvider.ItemTagProvider {
                 .add(DecoBlocks.ENDER_ROSE.asItem())
                 .add(DecoBlocks.PAEONIA.asItem())
                 .add(DecoBlocks.LAVENDER.asItem())
-                .add(DecoBlocks.ANCIENT_ROSE.asItem());
+                .add(DecoBlocks.ANCIENT_ROSE.asItem())
+                .add(DecoBlocks.BLUE_HIBISCUS.asItem())
+                .add(DecoBlocks.BROMELIAD.asItem());
 
         getOrCreateTagBuilder(DecoTags.Items.WATER_ITEMS)
                 .add(Items.WATER_BUCKET)
