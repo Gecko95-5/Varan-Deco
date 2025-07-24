19576a728f26e487398708164ac859560b786b8e
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2025-07-23 22:00:36 -0300

    1.3.D-(Bubble Elevator Hotfix)

--- src/main/java/net/gecko/varandeco/block/bubblelevatorblocks/BubbleElevatorBubbleBlock.java
+++ src/main/java/net/gecko/varandeco/block/bubblelevatorblocks/BubbleElevatorBubbleBlock.java
@@ -14,9 +14,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.block.WireOrientation;
 import net.minecraft.world.tick.ScheduledTickView;
+import org.jetbrains.annotations.Nullable;
 
 public class BubbleElevatorBubbleBlock extends Block {
 
@@ -46,6 +47,17 @@ public class BubbleElevatorBubbleBlock extends Block {
 
         return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
     }
+
+    @Override
+    protected void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, @Nullable WireOrientation wireOrientation, boolean notify) {
+        if (!world.isClient) {
+            return;
+        }
+            if (world.isReceivingRedstonePower(pos)) {
+                    world.setBlockState(pos, DecoBlocks.BUBBLE_ELEVATOR_BLOCK_MAGMA.getDefaultState(), Block.NOTIFY_LISTENERS);
+                }
+            }
+
 
     @Override
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
