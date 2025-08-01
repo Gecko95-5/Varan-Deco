9815ac8c80e98c4e9691f7c12c7d5b06cf344744
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2025-08-01 15:42:16 -0300

    1.4.A-(Fixes)

--- src/main/java/net/gecko/varandeco/block/bubblelevatorblocks/BubbleStairBlock.java
+++ src/main/java/net/gecko/varandeco/block/bubblelevatorblocks/BubbleStairBlock.java
@@ -3,6 +3,7 @@ package net.gecko.varandeco.block.bubblelevatorblocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.StairsBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -13,7 +14,10 @@ public class BubbleStairBlock extends StairsBlock {
 
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-        if (!entity.bypassesSteppingEffects()) {
+        if (!entity.bypassesSteppingEffects() && entity.isOnFire()) {
+            if (entity.isPlayer()) {
+                entity.playSound(SoundEvents.BLOCK_FIRE_EXTINGUISH, 1.0f, 1.0f);
+            }
             entity.extinguish();
         }
 
