1ba542899b8a41b105a887e6862f6839dc61898e
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2026-01-10 12:55:51 -0400

    2.0.0-(Mushroom Sign Patch)

--- src/main/java/net/gecko/varandeco/block/DecoBlocks.java
+++ src/main/java/net/gecko/varandeco/block/DecoBlocks.java
@@ -2400,7 +2400,7 @@ public class DecoBlocks {
     public static final Block ACACIA_PALLET_DOOR = registerBlock("acacia_pallet_door",
             new DoorBlock(FabricBlockSettings.copyOf(Blocks.ACACIA_DOOR).nonOpaque(), BlockSetType.ACACIA));
     public static final Block DARK_OAK_PALLET_DOOR = registerBlock("dark_oak_pallet_door",
-            new DoorBlock(FabricBlockSettings.copyOf(Blocks.ACACIA_DOOR).nonOpaque(), BlockSetType.DARK_OAK));
+            new DoorBlock(FabricBlockSettings.copyOf(Blocks.DARK_OAK_DOOR).nonOpaque(), BlockSetType.DARK_OAK));
     public static final Block MANGROVE_PALLET_DOOR = registerBlock("mangrove_pallet_door",
             new DoorBlock(FabricBlockSettings.copyOf(Blocks.MANGROVE_DOOR).nonOpaque(), BlockSetType.MANGROVE));
     public static final Block CRIMSON_PALLET_DOOR = registerBlock("crimson_pallet_door",
@@ -2434,7 +2434,7 @@ public class DecoBlocks {
     public static final Block ACACIA_PALLET_TRAPDOOR = registerBlock("acacia_pallet_trapdoor",
             new TrapdoorBlock(FabricBlockSettings.copyOf(Blocks.ACACIA_TRAPDOOR).nonOpaque(), BlockSetType.ACACIA));
     public static final Block DARK_OAK_PALLET_TRAPDOOR = registerBlock("dark_oak_pallet_trapdoor",
-            new TrapdoorBlock(FabricBlockSettings.copyOf(Blocks.ACACIA_TRAPDOOR).nonOpaque(), BlockSetType.DARK_OAK));
+            new TrapdoorBlock(FabricBlockSettings.copyOf(Blocks.DARK_OAK_TRAPDOOR).nonOpaque(), BlockSetType.DARK_OAK));
     public static final Block MANGROVE_PALLET_TRAPDOOR = registerBlock("mangrove_pallet_trapdoor",
             new TrapdoorBlock(FabricBlockSettings.copyOf(Blocks.MANGROVE_TRAPDOOR).nonOpaque(), BlockSetType.MANGROVE));
     public static final Block CRIMSON_PALLET_TRAPDOOR = registerBlock("crimson_pallet_trapdoor",
