789a35586078e181cbfa4f5881254b21fcf69be2
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2026-02-13 22:25:47 -0400

    2.1.0-(2.1 Finished)

---
+++ src/main/java/net/gecko/varandeco/block/nature/flowers/DesertFlowerBlock.java
@@ -0,0 +1,20 @@
+package net.gecko.varandeco.block.nature.flowers;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FlowerBlock;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+
+public class DesertFlowerBlock extends FlowerBlock {
+
+    public DesertFlowerBlock(StatusEffect suspiciousStewEffect, int effectDuration, Settings settings) {
+        super(suspiciousStewEffect, effectDuration, settings);
+    }
+
+    @Override
+    protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+        return floor.isIn(BlockTags.DEAD_BUSH_MAY_PLACE_ON);
+    }
+}
