5db483c3abe771c2043e075d8dcb5c0965dd05c2
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2026-02-17 13:14:36 -0400

    2.1.0-(Flammable Flowers)

--- src/main/java/net/gecko/varandeco/util/DecoFlammableBlocks.java
+++ src/main/java/net/gecko/varandeco/util/DecoFlammableBlocks.java
@@ -69,6 +69,10 @@ public class DecoFlammableBlocks {
         registry.add(DecoBlocks.NIGHTSHADE,60, 100);
         registry.add(DecoBlocks.ENDERSHADE,60, 100);
 
+        registry.add(DecoBlocks.DEAD_EYE_DAISY,60, 100);
+        registry.add(DecoBlocks.POPPED_BLUET,60, 100);
+        registry.add(DecoBlocks.ALPINE_POPPY,60, 100);
+
         registry.add(DecoTags.Blocks.LOG_STAIRS,5,5);
         registry.add(DecoTags.Blocks.LOG_SLABS,5,5);
         registry.add(DecoTags.Blocks.LOG_WALLS,5,5);
