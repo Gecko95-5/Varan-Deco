7c416b015df85d0016f2b3d61397c6d6218e41b7
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2026-02-22 18:54:26 -0400

    2.2.0-(2.2 Finished)

--- src/main/java/net/gecko/varandeco/datagen/DecoRecipeGeneratorTwo.java
+++ src/main/java/net/gecko/varandeco/datagen/DecoRecipeGeneratorTwo.java
@@ -13,7 +13,6 @@ import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.util.Identifier;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
@@ -2680,6 +2679,33 @@ public class DecoRecipeGeneratorTwo extends FabricRecipeProvider {
                 .criterion(RecipeProvider.hasItem(DecoBlocks.CRACKED_BLUE_NETHER_BRICKS),
                         RecipeProvider.conditionsFromItem(DecoBlocks.CRACKED_BLUE_NETHER_BRICKS))
                 .offerTo(exporter, new Identifier(RecipeProvider.getRecipeName(DecoBlocks.CRACKED_BLUE_NETHER_BRICK_FENCE_GATE)));
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC,Items.MAGENTA_DYE)
+                .input(DecoBlocks.ENDER_ROSE)
+                .group("magenta_dye")
+                .criterion(RecipeProvider.hasItem(DecoBlocks.ENDER_ROSE),
+                        RecipeProvider.conditionsFromItem(DecoBlocks.ENDER_ROSE))
+                .offerTo(exporter, new Identifier("magenta_dye_from_ender_rose"));
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC,Items.MAGENTA_DYE,4)
+                .input(DecoBlocks.ENDER_ROSE_BUSH)
+                .group("magenta_dye")
+                .criterion(RecipeProvider.hasItem(DecoBlocks.ENDER_ROSE_BUSH),
+                        RecipeProvider.conditionsFromItem(DecoBlocks.ENDER_ROSE_BUSH))
+                .offerTo(exporter, new Identifier("magenta_dye_from_ender_rose_bush"));
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC,Items.PINK_DYE,4)
+                .input(DecoBlocks.MIGHTY_LAVENDER)
+                .group("pink_dye")
+                .criterion(hasItem(DecoBlocks.MIGHTY_LAVENDER),
+                        conditionsFromItem(DecoBlocks.MIGHTY_LAVENDER))
+                .offerTo(exporter, new Identifier("pink_dye_from_mighty_lavender"));
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC,Items.PINK_DYE)
+                .input(DecoItems.MIGHTY_LAVENDER_FLOWER)
+                .group("pink_dye")
+                .criterion(hasItem(DecoItems.MIGHTY_LAVENDER_FLOWER),
+                        conditionsFromItem(DecoItems.MIGHTY_LAVENDER_FLOWER))
+                .offerTo(exporter, new Identifier("pink_dye_from_mighty_lavender_flower"));
     }
     public static void offerLadderVariantRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible woodInput) {
         ShapedRecipeJsonBuilder.create(RecipeCategory.DECORATIONS,output,3)
