a4c99cca69dc21594bd44c42b7bdee394a6f93ff
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2026-03-15 16:50:19 -0300

    2.3.0-(Loot Tables & Custom Trades)

--- src/main/java/net/gecko/varandeco/util/DecoLootTableModifiers.java
+++ src/main/java/net/gecko/varandeco/util/DecoLootTableModifiers.java
@@ -3,6 +3,7 @@ package net.gecko.varandeco.util;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.gecko.varandeco.block.DecoBlocks;
 import net.gecko.varandeco.item.DecoItems;
+import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
@@ -21,8 +22,14 @@ public class DecoLootTableModifiers {
             Identifier.of("minecraft", "entities/enderman");
     private static final Identifier FORTRESS_ID =
             Identifier.of("minecraft", "chests/nether_bridge");
-    private static final Identifier SHIPWRECK_SUPPLY_ID =
+    private static final Identifier SHIPWRECK_SP_ID =
             Identifier.of("minecraft", "chests/shipwreck_supply");
+    private static final Identifier SHIPWRECK_TR_ID =
+            Identifier.of("minecraft", "chests/shipwreck_treasure");
+    private static final Identifier MINESHAFT_ID =
+            Identifier.of("minecraft", "chests/abandoned_mineshaft");
+    private static final Identifier CITY_ID =
+            Identifier.of("minecraft", "chests/ancient_city");
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) -> {
@@ -44,6 +51,41 @@ public class DecoLootTableModifiers {
 
                 tableBuilder.pool(poolBuilder.build());
             }
+            if (MINESHAFT_ID.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(UniformLootNumberProvider.create(0.0f, 1.0f))
+                        .with(ItemEntry.builder(DecoBlocks.IRON_CAP_MUSHROOM))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (CITY_ID.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(ItemEntry.builder(DecoBlocks.IRON_CAP_MUSHROOM))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (SHIPWRECK_TR_ID.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(UniformLootNumberProvider.create(2.0f, 3.0f))
+                        .with(ItemEntry.builder(DecoItems.BUBBLE_ORB))
+                        .with(ItemEntry.builder(Items.PRISMARINE_SHARD))
+                        .with(ItemEntry.builder(Items.PRISMARINE_CRYSTALS))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 2.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (SHIPWRECK_SP_ID.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(UniformLootNumberProvider.create(2.0f, 4.0f))
+                        .with(ItemEntry.builder(DecoItems.TALL_SEAGRASS))
+                        .with(ItemEntry.builder(Items.SEAGRASS))
+                        .apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
             if (ENDERMAN_ID.equals(key.getValue())) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
@@ -62,7 +104,7 @@ public class DecoLootTableModifiers {
 
                 tableBuilder.pool(poolBuilder.build());
             }
-            if (SHIPWRECK_SUPPLY_ID.equals(key.getValue())) {
+            if (SHIPWRECK_SP_ID.equals(key.getValue())) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(UniformLootNumberProvider.create(1.0f,2.0f))
                         .with(ItemEntry.builder(DecoBlocks.DRIFTWOOD_LOG)
