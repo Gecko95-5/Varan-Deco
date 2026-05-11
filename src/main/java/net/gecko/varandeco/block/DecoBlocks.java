f7f712e701089d29d41a5608e489b3fee89a3901
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2026-05-11 00:07:37 -0300

    2.4.0-(2.4 Finished)

--- src/main/java/net/gecko/varandeco/block/DecoBlocks.java
+++ src/main/java/net/gecko/varandeco/block/DecoBlocks.java
@@ -2089,7 +2089,7 @@ public class DecoBlocks {
     public static final Block POTTED_PUFFY_DANDELION = registerBlockWithoutItem("potted_puffy_dandelion",
             new FlowerPotBlock(PUFFY_DANDELION,FabricBlockSettings.copyOf(Blocks.POTTED_DANDELION)));
     public static final Block TAIGA_DANDELION = registerBlock("taiga_dandelion",
-            new PuffyDandelionBlock(StatusEffects.INSTANT_HEALTH, 1,FabricBlockSettings.copyOf(Blocks.DANDELION)));
+            new FlowerBlock(StatusEffects.INSTANT_HEALTH, 1,FabricBlockSettings.copyOf(Blocks.DANDELION)));
     public static final Block POTTED_TAIGA_DANDELION = registerBlockWithoutItem("potted_taiga_dandelion",
             new FlowerPotBlock(TAIGA_DANDELION,FabricBlockSettings.copyOf(Blocks.POTTED_DANDELION)));
 
