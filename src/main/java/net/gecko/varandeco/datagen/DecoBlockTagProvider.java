f6851f744872db44630799bff17516337193a7d7
Gecko95-5 <153052131+Gecko95-5@users.noreply.github.com>
2026-05-12 19:04:15 -0300

    2.4.0-(Sands)

--- src/main/java/net/gecko/varandeco/datagen/DecoBlockTagProvider.java
+++ src/main/java/net/gecko/varandeco/datagen/DecoBlockTagProvider.java
@@ -2711,5 +2711,9 @@ public class DecoBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(DecoBlocks.HANGING_PURPLE_LAMP)
                 .add(DecoBlocks.HANGING_MAGENTA_LAMP)
                 .add(DecoBlocks.HANGING_PINK_LAMP);
+
+        getOrCreateTagBuilder(BlockTags.SAND)
+                .add(DecoBlocks.WHITE_SAND)
+                .add(DecoBlocks.BLACK_SAND);
     }
 }
